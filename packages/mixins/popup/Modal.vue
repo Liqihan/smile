080187fb6eb76f3765793e1fa370ce7045982597
李奇涵 <liqihan@benditoutiao.com>
2018-01-24 23:07:23 +0800

    popup basic methods

---
+++ packages/mixins/popup/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <transition name="s-fade">
+        <div
+            v-show="visible"
+            class="s-modal"
+            :class="className"
+            :style="style"
+            @touchmove.prevent.stop
+            @click="$emit('click', $event)"
+        >
+
+        </div>
+    </transition>
+</template>
+<script>
+export default {
+    name: "s-modal",
+    props: {
+        visible: Boolean,
+        zIndex: Number,
+        className: String,
+        customStyle: Object
+    },
+    computed: {
+        style() {
+            return {
+                zIndex: this.zIndex,
+                ...this.customStyle
+            };
+        }
+    }
+};
+</script>
---
+++ packages/mixins/popup/manager.js
@@ -0,0 +1,86 @@
+import Vue from "vue";
+import Modal from "./Modal";
+import context from "./context";
+const modalDefaultConfig = {
+    className: "",
+    customStyle: {}
+};
+const manager = {
+    getModal() {
+        let { modal } = context;
+        if (!modal) {
+            const ModalConstructor = Vue.extend(Modal);
+            modal = new ModalConstructor({
+                el: document.createElement("div")
+            });
+            modal.$on("click", () => {
+                manager.onClickOverlay();
+            });
+            context.modal = modal;
+        }
+        return modal;
+    },
+    onClickOverlay() {
+        const { top } = context;
+        if (top) {
+            const { instance } = top;
+            if (instance && instance.closeOnClickOverlay) {
+                instance.close();
+            }
+        }
+    },
+    openModal(instance, config) {
+        const { id, dom } = config;
+        const exist = context.stack.some(item => item.id === id);
+
+        if (!exist) {
+            const targetNode =
+                dom && dom.parentNode && dom.parentNode.nodeType !== 11
+                    ? dom.parentNode
+                    : document.body;
+            context.stack.push({ instance, id, config, targetNode });
+            this.ç();
+        }
+    },
+    openModal(instance, config) {
+        const { id, dom } = config;
+        const exist = context.stack.some(item => item.id === id);
+
+        if (!exist) {
+            const targetNode =
+                dom && dom.parentNode && dom.parentNode.nodeType !== 11
+                    ? dom.parentNode
+                    : document.body;
+            context.stack.push({ instance, id, config, targetNode });
+            this.updateModal();
+        }
+    },
+    closeModal(id) {
+        const { stack } = context;
+        if (stack.length) {
+            if (context.top.id === id) {
+                stack.pop();
+                this.updateModal();
+            } else {
+                context.stack = stack.filter(item => item.id !== id);
+            }
+        }
+    },
+    updateModal() {
+        const modal = this.getModal();
+        if (modal.$el.parentNode) {
+            modal.visible = false;
+        }
+        if (context.top) {
+            const { targetNode, config } = context.top;
+            targetNode.appendChild(modal.$el);
+            Object.assign(modal, {
+                ...modalDefaultConfig,
+                ...config,
+                visible: true
+            });
+        }
+    }
+};
+
+export default manager;
