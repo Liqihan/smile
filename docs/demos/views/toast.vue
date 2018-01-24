2c0cb6e421087dbd87db2119948ba7f27029ece5
李奇涵 <liqihan@benditoutiao.com>
2018-01-24 19:47:45 +0800

    toast组件

--- docs/demos/views/toast.vue
+++ docs/demos/views/toast.vue
@@ -3,20 +3,45 @@
         <demo-block title="sss">
             <s-button @click="showToast">默认toast</s-button>
         </demo-block>
+        <demo-block title="sss">
+            <s-button @click="showLoadingToast">loadingtoast</s-button>
+        </demo-block>
+        <demo-block title="高级用法">
+            <s-button @click="showCustomizedToast">高级</s-button>
+        </demo-block>
     </section>
 </template>
 <script>
-import {Toast} from 'packages'
+import { Toast } from "packages";
 export default {
     name: "demo-toast",
     data() {
         return {};
     },
-    mounted () {
-    },
+    mounted() {},
     methods: {
-        showToast () {
-            Toast('11111');
+        showToast() {
+            Toast("11111");
+        },
+        showLoadingToast() {
+            Toast.loading({ mask: true });
+        },
+        showCustomizedToast(duration) {
+            const toast = Toast.loading({
+                duration: 0,
+                forbidClick: true,
+                message: `倒计时3`
+            });
+            let second = 3;
+            const timer = setInterval(() => {
+                second--;
+                if (second) {
+                    toast.message = `倒计时${second}`
+                } else {
+                    clearInterval(timer);
+                    Toast.clear();
+                }
+            }, 1000);
         }
     }
 };
