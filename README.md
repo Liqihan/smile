9f0323cc0efe860949d83edd50f9a86dda1cbf16
李奇涵 <liqihan@benditoutiao.com>
2018-02-04 23:46:17 +0800

    popup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # smile
+
+A Vue.js 2.0 Mobile UI reference youzan vant;just a personal project for practice and try to learn more about Vue.js
---
+++ packages/popup/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <transition :name="currentTransition">
+        <div v-show="value" class="s-popup" :class="{ [`s-popup--${position}`]: position }">
+            <slot />
+        </div>
+    </transition>ÎÎ
+</template>
+<script>
+import { create } from '../utils';
+import Popup from '../mixins/popup';
+export default {
+    name: "s-popup",
+    data() {
+        return {};
+    }
+};
+</script>
+<style lang="css">
+</style>
